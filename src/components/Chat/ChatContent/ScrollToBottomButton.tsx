172b46f56ed8ee0f8c61b55b515796730404ae73
myskichs1 <myskichs1@gmail.com>
2023-08-17 10:24:50 +0900

    return to the point

--- src/components/Chat/ChatContent/ScrollToBottomButton.tsx
+++ src/components/Chat/ChatContent/ScrollToBottomButton.tsx
@@ -7,18 +7,13 @@ const ScrollToBottomButton = React.memo(() => {
   const scrollToBottom = useScrollToBottom();
   const [atBottom] = useAtBottom();
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    scrollToBottom();
-  };
-
   return (
     <button
       className={`cursor-pointer absolute right-6 bottom-[60px] md:bottom-[60px] z-10 rounded-full border border-gray-200 bg-gray-50 text-gray-600 dark:border-white/10 dark:bg-white/10 dark:text-gray-200 ${
         atBottom ? 'hidden' : ''
       }`}
       aria-label='scroll to bottom'
-      onClick={handleClick}
+      onClick={scrollToBottom}
     >
       <DownArrow />
     </button>
